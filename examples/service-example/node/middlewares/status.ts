4eb96264d06a0ab05d07c26d1646c6c05eca1cb8
Bruno Moreira <bruno@brunormoreira.com>
2023-11-02 10:16:57 -0300

    Add service-example

---
+++ examples/service-example/node/middlewares/status.ts
@@ -0,0 +1,29 @@
+export async function status(ctx: Context, next: () => Promise<any>) {
+  const {
+    state: { code },
+    clients: { status: statusClient },
+  } = ctx
+
+  console.info('logger: %s', ctx.logger)
+
+  console.info('Received code:', code)
+
+  const statusResponse = await statusClient.getStatus(code)
+
+  console.info('Status response:', statusResponse)
+
+  const {
+    headers,
+    data,
+    status: responseStatus,
+  } = await statusClient.getStatusWithHeaders(code)
+
+  console.info('Status headers', headers)
+  console.info('Status data:', data)
+
+  ctx.status = responseStatus
+  ctx.body = data
+  ctx.set('Cache-Control', headers['cache-control'])
+
+  await next()
+}
