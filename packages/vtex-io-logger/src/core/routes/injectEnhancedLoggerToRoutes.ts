cff2ae41c8b5aeeefa193bcd59f920408bb4738a
Bruno Moreira <bruno@brunormoreira.com>
2023-11-02 11:19:12 -0300

    Refactor logger injection middleware in routes

--- packages/vtex-io-logger/src/core/routes/injectEnhancedLoggerToRoutes.ts
+++ packages/vtex-io-logger/src/core/routes/injectEnhancedLoggerToRoutes.ts
@@ -1,17 +1,11 @@
-import type {
-  IOClients,
-  ParamsContext,
-  RecorderState,
-  ServiceConfig,
-} from "@vtex/api";
+import type { ServiceConfig } from "@vtex/api";
 import { ILogger } from "../../types/logger";
 import { enhancedLoggerInjectionMiddlewareFactory } from "../middlewares/enhancedLoggerInjectionMiddlewareFactory";
 
-export function injectEnhancedLoggerToRoutes<
-  T extends IOClients,
-  U extends RecorderState,
-  V extends ParamsContext
->(routes: ServiceConfig["routes"], { logger }: { logger?: ILogger }) {
+export function injectEnhancedLoggerToRoutes(
+  routes: ServiceConfig["routes"],
+  { logger }: { logger?: ILogger }
+) {
   if (!routes) {
     return routes;
   }
@@ -23,11 +17,9 @@ export function injectEnhancedLoggerToRoutes<
   const enhancedRoutes: typeof routes = {};
 
   for (const [name, handler] of Object.entries(routes)) {
-    if (Array.isArray(handler)) {
-      enhancedRoutes[name] = [injectionLoggerMiddleware, ...handler];
-    } else {
-      enhancedRoutes[name] = [injectionLoggerMiddleware, handler];
-    }
+    enhancedRoutes[name] = Array.isArray(handler)
+      ? [injectionLoggerMiddleware, ...handler]
+      : [injectionLoggerMiddleware, handler];
   }
 
   return enhancedRoutes;
---
+++ packages/vtex-io-logger/tests/core/routes/injectEnhancedLoggerToRoutes.spec.ts
@@ -0,0 +1,52 @@
+import { RouteHandler } from "@vtex/api";
+import { enhancedLoggerInjectionMiddlewareFactory } from "../../../src/core/middlewares/enhancedLoggerInjectionMiddlewareFactory";
+import { injectEnhancedLoggerToRoutes } from "../../../src/core/routes/injectEnhancedLoggerToRoutes";
+import { ILogger } from "../../../src/types/logger";
+
+jest.mock(
+  "../../../src/core/middlewares/enhancedLoggerInjectionMiddlewareFactory"
+);
+
+describe("injectEnhancedLoggerToRoutes", () => {
+  const mockLogger = {
+    info: jest.fn(),
+    error: jest.fn(),
+    debug: jest.fn(),
+    warn: jest.fn(),
+  } as ILogger;
+
+  const mockLoggerMiddleware = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (enhancedLoggerInjectionMiddlewareFactory as jest.Mock).mockReturnValue(
+      mockLoggerMiddleware
+    );
+  });
+
+  it("returns the same routes when no routes are provided", () => {
+    const result = injectEnhancedLoggerToRoutes(undefined, {
+      logger: mockLogger,
+    });
+
+    expect(result).toBe(undefined);
+  });
+
+  it("returns enhanced routes when routes are provided and the handler is an array", () => {
+    const routes = { testRoute: [jest.fn(), jest.fn()] };
+
+    const result = injectEnhancedLoggerToRoutes(routes, { logger: mockLogger });
+
+    const testRouteHandlers = result?.testRoute as RouteHandler[];
+    expect(testRouteHandlers[0]).toBe(mockLoggerMiddleware);
+  });
+
+  it("returns enhanced routes when routes are provided and the handler is a function", () => {
+    const routes = { testRoute: jest.fn() };
+
+    const result = injectEnhancedLoggerToRoutes(routes, { logger: mockLogger });
+
+    const testRouteHandlers = result?.testRoute as RouteHandler[];
+    expect(testRouteHandlers[0]).toBe(mockLoggerMiddleware);
+  });
+});
