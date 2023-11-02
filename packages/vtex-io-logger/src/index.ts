b6a50b711449fde05e176a33c93ba4f1a2921731
Bruno Moreira <bruno@brunormoreira.com>
2023-11-02 16:50:12 -0300

    Add with enhanced logger

--- packages/vtex-io-logger/src/index.ts
+++ packages/vtex-io-logger/src/index.ts
@@ -1,12 +1,25 @@
-import type {
-  IOClients,
-  ParamsContext,
-  RecorderState,
-  Service,
-} from "@vtex/api";
+import type { IOClients, ParamsContext, RecorderState } from "@vtex/api";
+import { Service } from "@vtex/api";
+import { injectEnhancedLoggerToRoutes } from "./core/injection/injectEnhancedLoggerToRoutes";
+import { instrumentRoutes } from "./core/instrumentation/routes/instrumentRoutes";
+import { ILogger } from "./types/logger";
 
-export function withLogger<
-  T extends IOClients,
-  U extends RecorderState,
-  V extends ParamsContext
->(service: Service<T, U, V>) {}
+interface Options {
+  logger?: ILogger;
+}
+
+export function withEnhancedLogger(
+  service: Service<IOClients, RecorderState, ParamsContext>,
+  { logger }: Options
+) {
+  const { config } = service;
+
+  let routes = injectEnhancedLoggerToRoutes(config.routes, { logger });
+
+  routes = instrumentRoutes(routes);
+
+  return new Service({
+    ...config,
+    routes,
+  });
+}
