8ebdfe2d0d3f7584f66f6f9abe7af73fad65cbc0
Bruno Moreira <bruno@brunormoreira.com>
2023-11-02 23:56:34 -0300

    Add pino logger to example

--- examples/custom-logger/node/index.ts
+++ examples/custom-logger/node/index.ts
@@ -2,16 +2,17 @@ import type {
   ClientsConfig,
   ParamsContext,
   RecorderState,
-  ServiceContext
-} from '@vtex/api'
-import { LRUCache, Service, method } from '@vtex/api'
+  ServiceContext,
+} from "@vtex/api";
+import { LRUCache, Service, method } from "@vtex/api";
 
-import { withEnhancedLogger } from '@sunstone/vtex-io-logger'
-import { Clients } from './clients'
-import { status } from './middlewares/status'
-import { validate } from './middlewares/validate'
+import { withEnhancedLogger } from "@sunstone/vtex-io-logger";
+import { Clients } from "./clients";
+import logger from "./lib/logger";
+import { status } from "./middlewares/status";
+import { validate } from "./middlewares/validate";
 
-const TIMEOUT_MS = 5000
+const TIMEOUT_MS = 5000;
 
 // Create a LRU memory cache for the Status client.
 // The 'max' parameter sets the size of the cache.
@@ -19,9 +20,9 @@ const TIMEOUT_MS = 5000
 // Note that the response from the API being called must include an 'etag' header
 // or a 'cache-control' header with a 'max-age' value. If neither exist, the response will not be cached.
 // To force responses to be cached, consider adding the `forceMaxAge` option to your client methods.
-const memoryCache = new LRUCache<string, any>({ max: 5000 })
+const memoryCache = new LRUCache<string, any>({ max: 5000 });
 
-metrics.trackCache('status', memoryCache)
+metrics.trackCache("status", memoryCache);
 
 // This is the configuration for clients available in `ctx.clients`.
 const clients: ClientsConfig<Clients> = {
@@ -38,7 +39,7 @@ const clients: ClientsConfig<Clients> = {
       memoryCache,
     },
   },
-}
+};
 
 declare global {
   // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
@@ -46,11 +47,11 @@ declare global {
     Clients,
     State,
     ParamsContext & { sunstone: { logger: any } }
-  >
+  >;
 
   // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
   interface State extends RecorderState {
-    code: number
+    code: number;
   }
 }
 
@@ -63,6 +64,6 @@ const service = new Service({
       GET: [validate, status],
     }),
   },
-})
+});
 
-export default withEnhancedLogger(service)
+export default withEnhancedLogger(service, { logger });
