ace1df1d8f33a0de74659d277dd4c1b7b4a8c3a6
Bruno Moreira <bruno@brunormoreira.com>
2023-11-06 13:48:22 -0300

    Refactor HTTP requests and update dependencies.

--- examples/custom-logger/node/clients/status.ts
+++ examples/custom-logger/node/clients/status.ts
@@ -1,23 +1,24 @@
-import type { InstanceOptions, IOContext, IOResponse } from '@vtex/api'
-import { ExternalClient } from '@vtex/api'
+import type { InstanceOptions, IOContext, IOResponse } from "@vtex/api";
+import { ExternalClient } from "@vtex/api";
 
 export default class Status extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
-    super('http://httpstat.us', context, options)
+    super("http://httpstat.us", context, options);
   }
 
   public async getStatus(status: number): Promise<string> {
     return this.http.get(status.toString(), {
-      metric: 'status-get',
-    })
+      metric: "status-get",
+    });
   }
 
   public async getStatusWithHeaders(
     status: number
   ): Promise<IOResponse<string>> {
     return this.http.getRaw(status.toString(), {
-      metric: 'status-get-raw',
-    })
+      metric: "status-get-raw",
+      validateStatus: () => true,
+    });
   }
 
   public async getStatusAndForceMaxAge(
@@ -26,7 +27,7 @@ export default class Status extends ExternalClient {
     return this.http.get(status.toString(), {
       // when using an LRUCache, this will force the response to be cached
       forceMaxAge: 5000,
-      metric: 'status-get-forceMaxAge',
-    })
+      metric: "status-get-forceMaxAge",
+    });
   }
 }
--- examples/custom-logger/node/middlewares/status.ts
+++ examples/custom-logger/node/middlewares/status.ts
@@ -7,18 +7,11 @@ export async function status(ctx: Context, next: () => Promise<any>) {
 
   logger.info(`Calling status with code ${code}`);
 
-  const statusResponse = await statusClient.getStatus(code);
-
-  console.info("Status response:", statusResponse);
-
   const {
     headers,
     data,
     status: responseStatus,
   } = await statusClient.getStatusWithHeaders(code);
-
-  console.info("Status headers", headers);
-  console.info("Status data:", data);
 
   ctx.status = responseStatus;
   ctx.body = data;
