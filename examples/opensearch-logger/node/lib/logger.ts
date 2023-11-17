ab8921766ff4965748096c9e0ecf096f55892456
Bruno Moreira <bruno@brunormoreira.com>
2023-11-17 11:28:22 -0300

    Adjust opensearch example

--- examples/opensearch-logger/node/lib/logger.ts
+++ examples/opensearch-logger/node/lib/logger.ts
@@ -3,33 +3,9 @@ import pino from "pino";
 import pinoOpenSearch from "pino-opensearch";
 
 const streamToOpenSearch = pinoOpenSearch({
-  index: "sunstone",
-  node: "https://search-sunstone-dev-pfod3lwksc4ch7iizua7iqdj5y.us-east-1.es.amazonaws.com",
-  "es-version": 7,
-  "flush-bytes": 1000,
-  "flush-interval": 5000,
-  auth: {
-    username: process.env.OPEN_SEARCH_USERNAME,
-    password: process.env.OPEN_SEARCH_PASSWORD,
-  },
+  // Your OpenSearch configurations
 });
 
-streamToOpenSearch.on("error", (err: any) => {
-  console.error("Error on pino-elasticsearch", err);
-});
-
-streamToOpenSearch.on("insertError", (err: any) => {
-  console.error("Error inserting document", err);
-});
-
-streamToOpenSearch.on("insert", (doc: any) => {
-  console.log("Inserted document", doc);
-});
-
-streamToOpenSearch.on("unknown", (line: any, error: any) => {
-  console.error("Unknown line", line, error);
-});
-
-const logger = pino({ level: "info" }, streamToOpenSearch);
+const logger = pino({ level: "error" }, streamToOpenSearch);
 
 export default logger;
