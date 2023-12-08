31e1b509594e5fc79ed558a487815c31cf58baea
v-hansen1 <vitor.yudi@gmail.com>
2023-12-08 19:14:00 -0300

    Update index.ts

--- packages/vtex-io-logger/src/index.ts
+++ packages/vtex-io-logger/src/index.ts
@@ -2,7 +2,7 @@ export * from "./api/logger-middleware";
 export * from "./api/with-full-logger";
 
 export type {
-  ContextWithOllie,
-  Ollie,
-  ParamsContextWithOllie,
+  ContextWithOllie, EventContextWithOllie, Ollie,
+  ParamsContextWithOllie
 } from "./types/ollie";
+
