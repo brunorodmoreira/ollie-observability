c5ce952682731ee34ba7cc89904bca7737fcb863
Hansen <vitor.yudi@gmail.com>
2024-02-26 20:07:38 -0300

    example

--- examples/event-opensearch-logger/node/events/example.ts
+++ examples/event-opensearch-logger/node/events/example.ts
@@ -1,5 +1,6 @@
 export async function example(ctx: StatusChangeContext) {
     console.log('RECEIVED EVENT', ctx.body)
+    console.log(ctx.ollie)
     ctx.ollie.logger.info('RECEIVED EVENT', ctx.body)
 
     return true
