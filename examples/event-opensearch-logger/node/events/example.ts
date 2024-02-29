4f3243dd4b5aa67805cc04e66bc0af2e069ff64d
Vitor Hansen <vitor.yudi@gmail.com>
2024-02-29 11:44:34 -0300

    adjust example

--- examples/event-opensearch-logger/node/events/example.ts
+++ examples/event-opensearch-logger/node/events/example.ts
@@ -1,6 +1,8 @@
-export async function example(ctx: StatusChangeContext) {
+import type { EventContextWithOllie } from "@ollie-dev/vtex-io-logger"
+
+export async function example(ctx: EventContextWithOllie) {
     console.log('RECEIVED EVENT', ctx.body)
-    console.log(ctx.ollie)
+    console.log(ctx.body)
     ctx.ollie.logger.info('RECEIVED EVENT', ctx.body)
 
     return true
